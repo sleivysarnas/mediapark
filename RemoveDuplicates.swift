f30032099e2809cf5e5e9c158baab9f37c425493
arnas <48261482+sleivysarnas@users.noreply.github.com>
2019-08-13 00:26:35 +0300

    Fix code styling

--- RemoveDuplicates.swift
+++ RemoveDuplicates.swift
@@ -2,10 +2,9 @@
 // Also, easy to apply to all Equatable elements, just need to change 'BinaryInteger' to 'Equatable'
 
 extension Array where Element: BinaryInteger {
+  
   func withoutDuplicates() -> [Element] {
-
     var filtered: [Element] = []
-
     for element in self {
       if !filtered.contains(element) {
         filtered.append(element)
