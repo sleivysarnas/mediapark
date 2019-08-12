881422f62d96b58759c7f92fdb11716841983940
arnas <48261482+sleivysarnas@users.noreply.github.com>
2019-08-13 00:25:00 +0300

    Fix indentation

--- ExcelColumnConverter.swift
+++ ExcelColumnConverter.swift
@@ -7,18 +7,18 @@ extension StringProtocol {
     var columnNumber = 0
     var multiplier = 1
     var asciiInt: Int
-
-//    We reverse the string, since we need to start multiplying from the end, not beggining.
+    
+    //    We reverse the string, since we need to start multiplying from the end, not beggining.
     for character in reversed() {
-//      If we cannot get ASCII representation of the character, we return nil.
+      //      If we cannot get ASCII representation of the character, we return nil.
       guard let asciiValue = character.asciiValue else { return nil }
-//      By default, above function returns UInt8 value, so we need to cast it to Int.
+      //      By default, above function returns UInt8 value, so we need to cast it to Int.
       asciiInt = Int(asciiValue)
-//      This guard is to make sure, that we are dealing with the uppercase alphabet only.
+      //      This guard is to make sure, that we are dealing with the uppercase alphabet only.
       guard asciiInt >= 65, asciiInt <= 90 else { return nil }
-//      We subtract 64 because ASCII values start from 65, not 1.
+      //      We subtract 64 because ASCII values start from 65, not 1.
       columnNumber += (asciiInt - 64) * multiplier
-//      We need to increase multiplier times 26 every character.
+      //      We need to increase multiplier times 26 every character.
       multiplier *= 26
     }
     return columnNumber
